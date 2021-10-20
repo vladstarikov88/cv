6e34f2d290b376a6373d155d2c085a57d5631838
Vladislav <vlad.starikov.88@gmail.com>
2021-10-20 18:00:20 +0300

    test

--- src/main.js
+++ src/main.js
@@ -6,8 +6,9 @@ import favicon from './assets/favicon.ico'
 createApp(App).mount('#app')
 
 
-
+console.log(favicon);
 const faviconBlock = document.createElement("style");
 faviconBlock.setAttribute("rel", "icon");
 faviconBlock.setAttribute("href", favicon);
 document.head.appendChild(faviconBlock);
+console.log(faviconBlock);
