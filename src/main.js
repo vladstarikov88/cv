3e76dda3c98d35fe267aa9f319e1fa772f0a0888
Vladislav <vlad.starikov.88@gmail.com>
2021-10-20 17:52:05 +0300

    update favicon

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import favicon from './assets/favicon.ico'
 
 createApp(App).mount('#app')
+
+
+
+const faviconBlock = document.createElement("style");
+faviconBlock.setAttribute("rel", "icon");
+faviconBlock.setAttribute("href", favicon);
+document.head.appendChild(faviconBlock);
