6f7bdd0099eb5914b82b88030743dca99af06b18
Vladislav <vlad.starikov.88@gmail.com>
2021-10-22 10:58:38 +0300

    update text

--- src/sections/ProfileSection.vue
+++ src/sections/ProfileSection.vue
@@ -2,13 +2,13 @@
   <div class="prose">
     <h2 class="section-headline print:hidden">About me</h2>
     <p>
-      I'm a passionate frontend developer, one who loves playing with new tech, has worked with, built, modern single-page apps, experience integrating with RESTful API backends, can speak JSON and not be afraid to look deep into the backend source code. I have a flair for design, enjoy implementing good looking UIs along with a UX. I spend my time learning cutting-edge technologies every day, striving to make the best developer out of myself.
+      I'm a passionate frontend developer, one who loves playing with new techs, building modern single-page apps, integrating RESTful APIs, a person who can speak JSON and is not afraid to look deep into the backend source code. I have a flair for design, so I enjoy implementing good looking UI. I spend my time learning cutting-edge technologies every day, striving to make myself better.
     </p>
     <p>
       I'm highly motivated in expanding my horizons day by day for better performance, testing and user experience.
     </p>
     <p>
-      I'm able to estimate time, manage a team, make great interfaces and give the expected result. I am always ready for challenging and interesting tasks.
+      My key skills also include time estimating, team management, making great interfaces and giving the expected results. I salut challenges and interesting tasks.
     </p>
   </div>
 </template>
