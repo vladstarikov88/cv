31804bdc4e8e726fed93e996150761f6263f6fc8
Vladislav <vlad.starikov.88@gmail.com>
2021-10-22 16:35:39 +0300

    update info

--- src/sections/ExpirienceSection.vue
+++ src/sections/ExpirienceSection.vue
@@ -9,9 +9,9 @@
       <template v-slot:title>Tlusty & Co. s.r.o.</template>
       <template v-slot:position>Lead frontend, UX designer</template>
       <template v-slot:description>
-        <div class="mb-1">I wrote a website builder to build web pages that use data from the company's internal api. The constructor allows you to create tables, questionnaires, forms, CRUD elements with advanced functionality. A lot of work has been done on architecture design using design patterns.</div>
-        <div class="mb-1">PWA was also developed, which allows an employee of the company to track time and manage the amount of work completed.</div>
-        <div>Using the above constructor, the ability to create polls was also developed. Therefore, employees using a clear interface can create polls in a couple of clicks.</div>
+        <div class="mb-1">I created a website builder to construct web pages that use data from the company's internal API. The builder allows users creating tables, questionnaires, forms, CRUD elements with advanced functionality. Much work was done on the architecture design created by means of design patterns.</div>
+        <div class="mb-1">Also, a PWA was developed, it allows company employees to track time and manage the amount of accomplished work.</div>
+        <div>Within the above builder, the feature to create polls was also developed. Therefore, employees can create polls in a couple of clicks using a clear interface.</div>
       </template>
       <!-- <template v-slot:period>Nov 2019 - now</template> -->
       <template v-slot:period>Mar 2020 - now</template>
@@ -24,8 +24,8 @@
       <template v-slot:title>Freelance</template>
       <template v-slot:position>Entrepreneur, developer</template>
       <template v-slot:description>
-        <div class="mb-1">After switching to freelance, I discovered a lot of skills. Searching for projects, communicating with customers, assigning roles, estimating deadlines, management, planning, reporting. In addition, I developed software products, learned the laws</div>
-        <div>Therefore, there are no unsolvable issues for me, since I have already understood that all issues can be solved.</div>
+        <div class="mb-1">After switching to freelance, I discovered a lot of skills. Searching for projects, communicating with customers, assigning roles, estimating deadlines, management, planning, reporting. In addition, I developed software products, learned more about the laws to become an entrepreneur.</div>
+        <div>Therefore, there are no insurmountable issues for me, since I have already understood that everything is resolvable.</div>
       </template>
       <template v-slot:period>Nov 2019 - now</template>
     </experience-description>
@@ -37,8 +37,8 @@
       <template v-slot:title>VendWay</template>
       <template v-slot:position>Frontend developer</template>
       <template v-slot:description>
-        <div class="mb-1">I wrote the client part for the vending machine, which allowed the user to select the desired position through the touchscreen on the screen and buy it through the payment terminal.</div>
-        <div>The administrative dashboard was also written to manage the state of the vending machine to view the balances, the total amount of purchases and other important indicators for the business.</div>
+        <div class="mb-1">I built the client part for the vending machine that allows users to select desired products via the touchscreen and buy them through the payment terminal.</div>
+        <div>The administrative dashboard was also developed to manage the state of the vending machine, to view the balance, the total amount of purchases and other important indicators for the business.</div>
       </template>
       <template v-slot:period>May 2019 - Nov 2019</template>
     </experience-description>
@@ -49,7 +49,7 @@
     >
       <template v-slot:title>WIS Software</template>
       <template v-slot:position>Frontend developer</template>
-      <template v-slot:description>I have worked on multiple projects as the company was outsourcing development. Worked with Rest API, websockets, graphQl, apollo. Created trigonometric shapes on canvas + d3.js. Wrote the client side to manage streaming services.</template>
+      <template v-slot:description>I worked on multiple projects as the company was focused on outsourcing development. I had experience with Rest API, websockets, graphQl, apollo. I learned how to create trigonometric shapes on canvas + d3.js. Also, I built the client side to manage streaming services.</template>
       <template v-slot:period>Nov 2018 - May 2019</template>
     </experience-description>
 
@@ -59,7 +59,7 @@
     >
       <template v-slot:title>PJSC CB "Center-invest"</template>
       <template v-slot:position>HTML developer</template>
-      <template v-slot:description>I have made up elements, pages, sites. I created a design system for the company's internal projects from scratch. Added responsive styles for a large-scale project with fixed width styles. </template>
+      <template v-slot:description>I made elements, pages, websites. I created a design system for the company's internal projects from scratch. I added responsive styles for a large-scale project with fixed width styles.</template>
       <template v-slot:period>May 2017 – Nov 2018</template>
     </experience-description>
   </section>
